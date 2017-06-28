c9e6a6454c153a144112e69d450bd0f7fcef74cc
Slava Petrov <slavik--petrov@yandex.ru>
2017-06-28 07:41:12 +0000

    Create endpoint for todos using express

--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-// save new smtg
 
-var newTodo = new Todo({
-  text: '    Edit the video            '
+var app = express();
+
+app.use(bodyParser.json()); 
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc)
+  }, (e) => {
+    res.status(400).send(e.message);
+  });
 });
 
-newTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save Todo');
-});
-
-var newUser = new User({
-  email: '   user@example.com   '
-});
-
-newUser.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2))
-}, (e) => {
-  console.log('Unable to save user', e)
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
