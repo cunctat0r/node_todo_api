03c97c39f1af45aa8a857736cf61bf7c0526a291
Slava Petrov <slavik--petrov@yandex.ru>
2017-06-28 07:18:47 +0000

    Extract models to separate files

--- server/server.js
+++ server/server.js
@@ -1,21 +1,7 @@
 var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
 // save new smtg
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false    
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
 
 var newTodo = new Todo({
   text: '    Edit the video            '
@@ -25,15 +11,6 @@ newTodo.save().then((doc) => {
   console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
   console.log('Unable to save Todo');
-});
-
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
 });
 
 var newUser = new User({
